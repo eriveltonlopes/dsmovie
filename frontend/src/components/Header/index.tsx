9d414c5a322106421c30d1152908dbfe7a778cf9
eriveltonlopes <enascimento2144@gmail.com>
2022-05-09 20:29:42 -0400

    navbar

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+export function Header() {
+  return (
+    <header >
+      <nav className='container'>
+        <div className='dsmovie-nav-content'>
+          <h1>DSMovie</h1>
+          <a href="https://github.com/eriveltonlopes">
+            <div className='dsmovie-contact-container'>
+              <GithubIcon />
+              <p className='dsmovie-contact-link'>/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
