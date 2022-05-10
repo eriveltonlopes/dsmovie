4c5b3e2d6c58170bf551fcd96618e03d4c18a840
eriveltonlopes <enascimento2144@gmail.com>
2022-05-09 20:45:00 -0400

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
-export function Header() {
+export function Navbar() {
   return (
     <header >
       <nav className='container'>
